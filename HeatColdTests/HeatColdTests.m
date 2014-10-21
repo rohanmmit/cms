d48813edcfb983640d3550cf5906f2a8a080c781
rohan mahajan <rohanm@rohans-MacBook-Pro-2.local>
2014-10-21 14:02:18 -0400

    Initial Commit

---
+++ HeatColdTests/HeatColdTests.m
@@ -0,0 +1,34 @@
+//
+//  HeatColdTests.m
+//  HeatColdTests
+//
+//  Created by rohan mahajan on 10/21/14.
+//  Copyright (c) 2014 rohan mahajan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HeatColdTests : XCTestCase
+
+@end
+
+@implementation HeatColdTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
